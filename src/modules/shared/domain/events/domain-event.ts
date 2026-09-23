5373d440131864ebdf442824f738c23639f367b2
Iyanda Dotun <olamidotun225@gmail.com>
2026-09-23 14:56:34 +0100

    fix(shared): update domain event base class

--- src/modules/shared/domain/events/domain-event.ts
+++ src/modules/shared/domain/events/domain-event.ts
@@ -1,37 +1,39 @@
 type DomainEventPayload = object;
 
-interface DomainEvent<TPayload extends DomainEventPayload = DomainEventPayload>
-{
-    readonly eventId: string;
-    readonly occurredAt: Date;
-    readonly aggregateId: string;
-    readonly eventName: string;
-    readonly correlationId: string;
-    readonly payload: TPayload;
+interface DomainEvent<
+  TPayload extends DomainEventPayload = DomainEventPayload,
+> {
+  readonly eventId: string;
+  readonly occurredAt: Date;
+  readonly aggregateId: string;
+  readonly eventName: string;
+  readonly correlationId: string;
+  readonly payload: TPayload;
 }
 
-abstract class BaseDomainEvent<TPayload extends DomainEventPayload>
-implements DomainEvent<TPayload> {
-    readonly eventId: string;
-    readonly occurredAt: Date;
-    readonly aggregateId: string;
-    readonly eventName: string;
-    readonly correlationId: string;
-    readonly payload: TPayload;
+abstract class BaseDomainEvent<
+  TPayload extends DomainEventPayload,
+> implements DomainEvent<TPayload> {
+  readonly eventId: string;
+  readonly occurredAt: Date;
+  readonly aggregateId: string;
+  readonly eventName: string;
+  readonly correlationId: string;
+  readonly payload: TPayload;
 
-    protected constructor(params: {
-        aggregateId: string;
-        eventName: string;
-        payload: TPayload;
-        correlationId: string;
-    }) {
-        this.eventId = crypto.randomUUID();
-        this.occurredAt = new Date();
-        this.aggregateId = params.aggregateId;
-        this.eventName = params.eventName;
-        this.correlationId = params.correlationId;
-        this.payload = params.payload;
-    }
+  protected constructor(params: {
+    aggregateId: string;
+    eventName: string;
+    payload: TPayload;
+    correlationId: string;
+  }) {
+    this.eventId = crypto.randomUUID();
+    this.occurredAt = new Date();
+    this.aggregateId = params.aggregateId;
+    this.eventName = params.eventName;
+    this.correlationId = params.correlationId;
+    this.payload = params.payload;
+  }
 }
 
 export type { DomainEvent, DomainEventPayload };
